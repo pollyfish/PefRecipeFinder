d3e9984a4d798b483d4da15bded74917e3065b65
Polly Fisher <polly.fisher@gmail.com>
2019-01-22 21:15:08 -0600

    more cx add 400 error helper

--- PuppyPef.js
+++ PuppyPef.js
@@ -28,19 +28,18 @@ var request = new XMLHttpRequest();
 
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-
 request.onload = function () {
-  // Begin accessing JSON data here
-  }
-}
-
-// Send request
-request.send();
 
 // Begin accessing JSON data here
 var data = JSON.parse(this.response);
-
+if (request.status >= 200 && request.status < 400) {
 data.forEach(movie => {
   // Log each movie's title
   console.log(movie.title);
 });
+} else {
+ console.log('error');
+}
+}
+ // Send request
+ request.send();
